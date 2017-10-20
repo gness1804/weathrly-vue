2fb588b4bc8192ee27f17e322441cb30e1bcd8c7
gness1804 <graham.nessler@gmail.com>
2017-10-20 09:16:02 -0500

    Create basic skeleton

---
+++ src/components/Landing.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="landing">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  data() {
+    return {
+      msg: 'I am the landing page.',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
