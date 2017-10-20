a5d2d051556a30374d0c2f028d26f5da19cd8ed1
gness1804 <graham.nessler@gmail.com>
2017-10-20 11:22:46 -0500

    Remove console log

--- src/helpers/zipIsValid.js
+++ src/helpers/zipIsValid.js
@@ -1,7 +1,6 @@
 // @flow
 
 const zipIsValid = (zip: string): boolean => {
-  console.log('zip:', zip);
   if (!zip || typeof parseInt(zip, 10) !== 'number' || zip.length !== 5) {
     return false;
   }
