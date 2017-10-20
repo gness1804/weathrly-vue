de768baa3d41d3be2dbc1fee41892f4582273190
gness1804 <graham.nessler@gmail.com>
2017-10-20 15:28:06 -0500

    Add styling for NoWeatherComponent

--- src/components/NoWeatherComponent.vue
+++ src/components/NoWeatherComponent.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="no-weather-component">
-    <p>No Weather to Show! Please enter either a US city and state or a valid 5 digit US zip code. Sorry, international locations are not currently supported.</p>
+  <div
+    class="no-weather-component"
+    v-bind:style="mainContainer"
+  >
+    <p
+      v-bind:style="mainText"
+    >
+    No Weather to Show! Please enter either a US city and state or a valid 5 digit US zip code. Sorry, international locations are not currently supported.
+    </p>
   </div>
 </template>
 
 <script>
+import styles from '../styles/NoWeatherComponent-styles';
+
 export default {
   name: 'NoWeatherComponent',
+  data() {
+    return {
+      mainContainer: styles.mainContainer,
+      mainText: styles.mainText,
+    };
+  },
 };
 </script>
 
